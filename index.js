badeef2003ef5b99410d9356d0b81758353a073f
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2021-09-24 20:38:26 -0500

    Add static images folder and basic routes

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Static folder with images
+app.use(express.static(__dirname + 'src/images'))
+
+// Home url
 app.get('/', (req,res) =>{
     res.send('This works!')
+})
+
+// Childs url
+app.get('/hijos', (req,res) =>{
+    res.send('Seccion de hijos')
+})
+
+// Parents urls
+app.get('/padres', (req,res) =>{
+    res.send('Seccion de hijos')
 })
 
 app.listen(port, () =>{
